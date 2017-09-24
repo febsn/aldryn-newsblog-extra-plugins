62e68a36dd354b11549032a7ec98a4f527728ad2
Fabian Lehner <fl@makonis.net>
2017-09-24 17:38:42 +0200

    v0.0.2, added setup files

---
+++ README.md
@@ -0,0 +1,29 @@
+aldryn-newsblog-extra-plugins
+=============================
+
+This projects contains extra plugins for the `aldryn-newsblog` blogging system
+for DjangoCMS. It requires `aldryn-newsblog` to be installed and setup correctly.
+
+# Configuration
+Plugins that show a list of articles can be configured to show template choices,
+inspired by `aldryn-events`. To add, for example, a template named `list`, save
+it at `templates/aldryn-newsblog/plugins/(template name)` and add the following
+to your `settings.py`:
+
+```` Python
+ALDRYN_NEWSBLOG_PLUGIN_STYLES = (
+    'list',
+)
+````
+
+# Plugins
+## NewsBlogTaggedArticlesPlugin
+Show `n` or all articles that are tagged with a certain tag.
+This plugin shows style choices as described in "Configuration".
+
+## NewsBlogTagRelatedPlugin
+Show `n`or all articles that are similarly tagged as the currently displayed
+article.
+
+This plugin only works correctly in a static placeholder on the aldryn_newsblog
+detail view.
---
+++ setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+# To register app to PyPI:
+# python setup.py register -r pypi
+#
+# NOTE: To update PyPI, tag the current release:
+#
+# First increment cache_tools/__init__.py
+# Then:
+# > git tag x.y.z -m "Version bump for PyPI"
+# > git push --tags origin master
+# Then:
+# > python setup.py sdist upload
+#
+
+from setuptools import setup, find_packages
+from aldryn_newsblog_extra_plugins import __version__
+
+
+INSTALL_REQUIRES = [
+    'aldryn-newsblog',
+    'six',
+]
+
+CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Programming Language :: Python',
+]
+
+setup(
+    name='aldryn-newsblog-extra-plugins',
+    version=__version__,
+    description='Extra plugins for aldryn-newsblog.',
+    author='Fabian Lehner',
+    author_email='fl@makonis.net',
+    url='https://github.com/febsn/aldryn-newsblog-extra-plugins',
+    packages=find_packages(),
+    install_requires=INSTALL_REQUIRES,
+    license='MIT',
+    platforms=['OS Independent'],
+    classifiers=CLASSIFIERS,
+    long_description=open('README.md').read(),
+    include_package_data=True,
+    zip_safe=False,
+)
